ba8bc6fdba2de9f38dbda7744dfe9242ca1bef31
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2024-01-28 16:17:52 +0100

    promesas

---
+++ 4.fs-promises.js
@@ -0,0 +1,15 @@
+const fs = require('node:fs');  //* fs por defecto no tienes promesas, solo callbacks
+const fs = require('node:fs/promises');
+
+// const { promisify } = require('node:util'); //* <--- importa promisify 
+// const readFile = promisify(fs.readFile); //* <--- fs.readFile era callback, ahora tenemos readFile que es promesa
+
+
+fs.readFile('./example.txt', 'utf-8')
+    .then(text => { console.log(text); })
+
+console.log('Haces mas cosas...'); // <--- antes que el callback
+
+// const text = fs.readFileSync('./example.txt', 'utf-8');
+
+// console.log(text);

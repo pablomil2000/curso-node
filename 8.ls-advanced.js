e2737dc921b6188bea55f815384360cb86429a38
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2024-01-28 19:21:41 +0100

    ls-advanced

---
+++ 8.ls-advanced.js
@@ -0,0 +1,43 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+const folder = process.argv[2] ?? '.';
+
+
+async function ls(folder) {
+    let files
+    try {
+        files = await fs.readdir(folder);
+        files.forEach(file => {
+            console.log(file);
+        });
+    } catch (error) {
+        console.log(`Error al leer el directorio ${folder}`);
+        process.exit(1);
+    }
+
+    const filesPromises = files.map(async file => {
+        const filePath = path.join(folder, file);
+        let stats;
+
+        try {
+            stats = await fs.stat(filePath);  //Informacion del archivo
+        } catch (error) {
+            console.log(`Error al leer el archivo ${filePath}`);
+            process.exit(1);
+        }
+
+        const isDirectory = stats.isDirectory()
+        const fileType = isDirectory ? 'd' : '-'
+        const fileSize = stats.size
+        const fileModified = stats.mtime.toLocaleString()
+
+        return `${fileType} ${file.padEnd(30)} ${fileSize.toString().padStart(6)} ${fileModified}`
+    })
+
+    const filesInfo = await Promise.all(filesPromises);
+
+    filesInfo.forEach(fileInfo => console.log(fileInfo))
+}
+
+
+ls(folder);

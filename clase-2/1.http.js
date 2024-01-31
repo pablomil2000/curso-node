6dfc298bcfe011cfc4c121bc48777f33ff18f8c0
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2024-01-31 20:55:16 +0100

    clase 2

---
+++ clase-2/1.http.js
@@ -0,0 +1,22 @@
+const http = require('node:http')
+const pc = require('picocolors')
+
+const desiredPort = process.env.PORT ?? 3000
+
+const procesarRequest = (req, res) => {
+  console.log('peticion recibida: ', pc.green(req.url));
+
+  if (req.url === '/') {
+    res.statusCode = 200 //* OK
+    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Charset', 'utf-8')
+    res.end('<h1>Bienvenido a mi servidor</h1>')
+  }
+}
+
+const server = http.createServer(procesarRequest)
+
+// Utiliza el primer puerto disponible
+server.listen(desiredPort, () => {
+  console.log(`Servidor escuchando en el puerto  http://localhost:${server.address().port}`)
+})

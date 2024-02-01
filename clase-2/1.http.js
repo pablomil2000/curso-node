eab147a583236b56af58f616df52e87022a6556c
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2024-02-01 19:56:41 +0100

    primero servidor

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -4,13 +4,23 @@ const pc = require('picocolors')
 const desiredPort = process.env.PORT ?? 3000
 
 const procesarRequest = (req, res) => {
-  console.log('peticion recibida: ', pc.green(req.url));
+  console.log('peticion recibida: ', pc.green(req.url))
 
   if (req.url === '/') {
     res.statusCode = 200 //* OK
     res.setHeader('Content-Type', 'text/html')
     res.setHeader('Charset', 'utf-8')
     res.end('<h1>Bienvenido a mi servidor</h1>')
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200 //* OK
+    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Charset', 'utf-8')
+    res.end('<h1>Contactanos</h1>')
+  } else {
+    res.statusCode = 404 //* Not Found
+    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Charset', 'utf-8')
+    res.end('<h1>404</h1>')
   }
 }
 

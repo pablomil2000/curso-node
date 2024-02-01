4403145817998bb791fff61cb9df28e4388716f1
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2024-02-01 20:07:50 +0100

    nodemon + cabeceras

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,6 +1,6 @@
 const http = require('node:http')
 const pc = require('picocolors')
-
+const fs = require('node:fs')
 const desiredPort = process.env.PORT ?? 3000
 
 const procesarRequest = (req, res) => {
@@ -16,6 +16,18 @@ const procesarRequest = (req, res) => {
     res.setHeader('Content-Type', 'text/html')
     res.setHeader('Charset', 'utf-8')
     res.end('<h1>Contactanos</h1>')
+  } else if (req.url === '/portada.png') {
+    fs.readFile('./portada.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500 //* Internal Server Error
+        res.setHeader('Content-Type', 'text/html')
+        res.end('<h1>Internal Server Error</h1>')
+      } else {
+        res.setHeader('Content-Type', 'image/png')
+        res.statusCode = 200 //* OK
+        res.end(data)
+      }
+    })
   } else {
     res.statusCode = 404 //* Not Found
     res.setHeader('Content-Type', 'text/html')

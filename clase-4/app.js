d22addc36ad7785f268f635d1002ca3a4a0e9f96
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2024-02-10 18:52:20 +0100

    inicio clase 4

---
+++ clase-4/app.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const crypto = require('node:crypto')
+const moviesJSON = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schema/movies')
+
+const app = express()
+app.disable('x-powered-by') // Deshabilita la cabecera X-Powered-By
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo')
+})
+
+app.get('/movies', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  const { genre } = req.query
+
+  if (genre) {
+    console.log(genre)
+    const filteredMovies = moviesJSON.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  return res.json(moviesJSON)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+
+  const movie = moviesJSON.find((movie) => movie.id === id)
+  if (movie) {
+    res.json(movie)
+  } else {
+    res.status(404).json({ error: 'Movie not found' })
+  }
+})
+
+app.post('/movies', (req, res) => {
+  // const { title, year, director, duration, poster, genre, rate } = req.body
+
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({
+      error: JSON.parse(result.error.message)
+    })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  //! Esto no es ApiRest
+  moviesJSON.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  // -- Validamos lo que llega en el body
+  const result = validatePartialMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({
+      error: JSON.parse(result.error.message)
+    })
+  }
+
+  // ? Existe la pelicula que queremos actualziar?
+  const { id } = req.params
+  const movieIndex = moviesJSON.findIndex((movie) => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ error: 'Movie not found' })
+  }
+
+  //* Actualizacion de la pelicula
+  const updatedMovie = {
+    ...moviesJSON[movieIndex],
+    ...result.data
+  }
+  console.log(movieIndex)
+  moviesJSON[movieIndex] = updatedMovie
+
+  return res.json(updatedMovie)
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto  http://localhost:${PORT}`)
+})

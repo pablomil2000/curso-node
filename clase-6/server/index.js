b5fddf919ee04f50c3a58ccdf0f165e682e6900e
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2024-02-11 11:30:20 +0100

    primer websocket

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -9,12 +9,21 @@ const app = express();
 const server = createServer(app)
 const io = new Server(server)
 
+io.on('connection', (socket) => {
+  console.log('Usuario conectado')
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado')
+  })
+})
+
+
 app.use(logger('dev'))  //logs de morgan
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')  //Cargar vista
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto  http://localhost:${PORT}`)
 })

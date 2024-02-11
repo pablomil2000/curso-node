5812267b8e2a9658d0620a4addaa990cd78793d9
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2024-02-11 08:11:53 +0100

    usar mysql

--- clase-5/models/mysql/movie.js
+++ clase-5/models/mysql/movie.js
@@ -1,9 +1,21 @@
-import { randomUUID } from 'node:crypto'
-import { readJSON } from '../../utils.js'
+import mysql from 'mysql2/promise'
+
+const config = {
+  host: 'localhost',
+  user: 'root',
+  port: 3306,
+  password: '',
+  database: 'node-curso'
+}
+
+const conect = await mysql.createConnection(config) // Conect to database
 
 export class MovieModel {
   static async getAll ({ genre }) {
+    const result = await conect.query(
+      'SELECT * FROM movies')
 
+    console.log(result)
   }
 
   static async getById ({ id }) {

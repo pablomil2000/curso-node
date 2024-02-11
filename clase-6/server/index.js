411fe26ed70348ecf15aa7aecdf5bd7c33f6f6b3
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2024-02-11 11:22:08 +0100

    server io

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const server = createServer(app)
+const io = new Server(server)
+
 app.use(logger('dev'))  //logs de morgan
 
 app.get('/', (req, res) => {
